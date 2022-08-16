51aed3d9a15e4915584d2d3ac765f01a7b99bd9e
Nek <privatenek@gmail.com>
2022-08-17 01:09:21 +0200

    verify your code

--- src/main/java/me/nek/ffabyne/DeathListener.java
+++ src/main/java/me/nek/ffabyne/DeathListener.java
@@ -8,33 +8,6 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class DeathListener implements Listener {
 
-    int max = 5;
-    int min = 1;
-    int range = max - min + 1;
-
-    /*
-     int rand = (int)(Math.random() * range) + min;
-            if (rand == 0){
-                Bukkit.broadcastMessage(ChatColor.AQUA + p.getName() + ChatColor.GRAY + " was destroyed by " + ChatColor.AQUA + k.getName() + ChatColor.GRAY  + " on " +  ChatColor.RED +k.getHealth());
-            } else if (rand == 1) {
-                Bukkit.broadcastMessage(ChatColor.AQUA + p.getName() + ChatColor.GRAY + " lost his teeth because " + ChatColor.AQUA + k.getName() + ChatColor.GRAY  + " on " +  ChatColor.RED +k.getHealth());
-
-            }
-            else if (rand == 2) {
-                Bukkit.broadcastMessage(ChatColor.AQUA + p.getName() + ChatColor.GRAY + " went underwater due to " + ChatColor.AQUA + k.getName() + ChatColor.GRAY  + " ob " +  ChatColor.RED +k.getHealth());
-
-            }
-            else if (rand == 3) {
-                Bukkit.broadcastMessage(ChatColor.AQUA + p.getName() + ChatColor.GRAY + " was cleaned by " + ChatColor.AQUA + k.getName() + ChatColor.GRAY  + " on " +  ChatColor.RED +k.getHealth());
-
-            }
-            else{
-                Bukkit.broadcastMessage(ChatColor.AQUA + p.getName() + ChatColor.GRAY + " died thanks to " + ChatColor.AQUA + k.getName() + ChatColor.GRAY  + " on " +  ChatColor.RED +k.getHealth());
-
-            }
-
-     */
-
     public void on(PlayerDeathEvent event){
         Player p = event.getEntity().getPlayer();
         Player k = event.getEntity().getKiller();
@@ -43,6 +16,7 @@ public class DeathListener implements Listener {
             p.getInventory().clear();
             p.sendTitle("GG, you died", "because " + k.getName() + " on " + k.getHealth(), 5, 20, 10);
             k.setHealth(20);
+            k.setSaturation(5);
 
 
         }else{
